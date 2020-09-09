0eacdb92f880571872ad0b567732fc5aaf8d05d6
masamichinakano <ma5sa9mi0chi9@gmail.com>
2020-09-09 20:58:21 +0900

    新規登録機能　単体テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname
-    email
-    password
-    password_confirmation
-    f_name
-    l_name
-    f_name_leader
-    l_name_leader
-    birthday
+    nickname {Faker::Name.name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    f_name {"山田"}
+    l_name { "太郎"}
+    f_name_leader {"ヤマダ"}
+    l_name_leader {"タロウ"}
+    birthday {Faker::Date.birthday}
   end
 end
