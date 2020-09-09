36b513596660f400c9556c430ed8c0fdc1a45a19
masamichinakano <ma5sa9mi0chi9@gmail.com>
2020-09-09 14:54:03 +0900

    新規登録時のエラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, :f_name, :l_name, :f_name_leader, :l_name_leader, :birthday, presence: true
+  
+
 end
